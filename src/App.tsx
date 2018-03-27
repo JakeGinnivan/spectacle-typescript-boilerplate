9929158a35c2eb1b244049c1d2463a3a165dea5f
Jake Ginnivan <jake.ginnivan@wanews.com.au>
2018-03-27 17:35:26 +0800

    Upgraded to latest spectacle

--- src/App.tsx
+++ src/App.tsx
@@ -6,31 +6,29 @@ import { BlockQuote, Cite, Deck, Heading, ListItem, List, Quote, Slide, Text } f
 // Import image preloader util
 import preloader from 'spectacle/lib/utils/preloader'
 
-// Import theme
 import createTheme from 'spectacle/lib/themes/default'
 
 // Require CSS
 require('normalize.css')
-require('spectacle/lib/themes/default/index.css')
-
-const images = {
-    // city: require('../assets/city.jpg'),
-}
-
-preloader(images)
 
 const theme = createTheme(
     {
         primary: 'white',
         secondary: '#1F2022',
         tertiary: '#03A9FC',
-        quartenary: '#CECECE'
+        quarternary: '#CECECE'
     },
     {
         primary: 'Montserrat',
         secondary: 'Helvetica'
     }
 )
+
+const images = {
+    // city: require('../assets/city.jpg'),
+}
+
+preloader(images)
 
 export default class Presentation extends React.Component {
     render() {
